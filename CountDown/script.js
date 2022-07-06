fd439d9949f8f3fdd7312b89c552e78aebcdf69a
almasim <myl4n1@gmail.com>
2022-07-06 19:20:44 +0200

    Done

--- CountDown/script.js
+++ CountDown/script.js
@@ -1,13 +1,30 @@
+const daysE1=document.getElementById('days');
+const hoursE1=document.getElementById('hours');
+const minutesE1=document.getElementById('minutes');
+const secondsE1=document.getElementById('seconds');
+
 const christmas='24 Dec 2022';
 
 function countdown(){
     const christmasDate=new Date(christmas);
     const currentDate=new Date();
-    const seconds=(christmasDate-currentDate)/1000;
-    const days= Math.floor(seconds/3600/24); 
-    const hours=Math.floor(seconds/3600)%24;
-    const minutes=Math.floor(seconds/3600)%24;
-    console.log(minutes)
+
+    const totalSeconds=(christmasDate-currentDate)/1000;
+    
+    const days= Math.floor(totalSeconds / 3600 / 24); 
+    const hours=Math.floor(totalSeconds / 3600) % 24;
+    const minutes=Math.floor(totalSeconds / 60) % 60;
+    const seconds=Math.floor(totalSeconds % 60);
+
+    daysE1.innerHTML=days;
+    hoursE1.innerHTML=formatTime(hours);
+    minutesE1.innerHTML=formatTime(minutes);
+    secondsE1.innerHTML=formatTime(seconds);
+    
+}
+
+function formatTime(time){
+    return time<10 ?(`0${time}`):time;
 }
 ///initial call
 setInterval(countdown,1000)
